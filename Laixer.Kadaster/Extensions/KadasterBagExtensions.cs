2363a729499e4ac029f02fbe1dc16476f7139a05
Yorick de Wid <yorick17@outlook.com>
2019-10-23 00:47:51 +0200

    Service extensions

---
+++ Laixer.Kadaster/Extensions/KadasterBagExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Laixer.Kadaster.Bag;
+using System;
+
+namespace Laixer.Kadaster.Extensions
+{
+    /// <summary>
+    /// Extensions on the kadaser bag registration.
+    /// </summary>
+    public static class KadasterBagExtensions
+    {
+        public static DesignationService DesignationService(this KadasterBag bag)
+        {
+            if (bag == null)
+            {
+                throw new ArgumentNullException(nameof(bag));
+            }
+
+            return bag.GetService(BagService.Designation) as DesignationService;
+        }
+
+        public static PremiseService PremiseService(this KadasterBag bag)
+        {
+            if (bag == null)
+            {
+                throw new ArgumentNullException(nameof(bag));
+            }
+
+            return bag.GetService(BagService.Premise) as PremiseService;
+        }
+
+        public static CityService CityService(this KadasterBag bag)
+        {
+            if (bag == null)
+            {
+                throw new ArgumentNullException(nameof(bag));
+            }
+
+            return bag.GetService(BagService.City) as CityService;
+        }
+    }
+}
