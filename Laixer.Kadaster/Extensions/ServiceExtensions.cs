b3c2705d38e0be032b3f5b5e53914ed933416ac4
Yorick de Wid <yorick17@outlook.com>
2019-10-25 15:29:03 +0200

    Chain operations together

--- Laixer.Kadaster/Extensions/ServiceExtensions.cs
+++ Laixer.Kadaster/Extensions/ServiceExtensions.cs
@@ -8,15 +8,29 @@ namespace Laixer.Kadaster
         /// <summary>
         /// Request public space from designation.
         /// </summary>
-        /// <param name="bagObject"></param>
-        /// <param name="bag"></param>
-        /// <returns></returns>
+        /// <param name="bagObject">Entity to extend.</param>
+        /// <param name="bag">BAG registrar.</param>
+        /// <returns><see cref="BagObject<PublicSpace>"/>.</returns>
         public static BagObject<PublicSpace> PublicSpace(this BagObject<Designation> bagObject, KadasterBag bag)
         {
             var publicSpaceService = bag.PublicSpaceService();
             var uri = bagObject.Value.Links.PublicSpace.Href;
-            var id = uri.Segments[4];
+            var id = uri.Segments[4]; // TODO: This may not always be true;
             return publicSpaceService.GetById(new BagId(id));
+        }
+
+        /// <summary>
+        /// Request city from designation.
+        /// </summary>
+        /// <param name="bagObject">Entity to extend.</param>
+        /// <param name="bag">BAG registrar.</param>
+        /// <returns><see cref="BagObject<City>"/>.</returns>
+        public static BagObject<City> City(this BagObject<PublicSpace> bagObject, KadasterBag bag)
+        {
+            var cityService = bag.CityService();
+            var uri = bagObject.Value.Links.City.Href;
+            var id = uri.Segments[4]; // TODO: This may not always be true;
+            return cityService.GetById(new BagId(id));
         }
     }
 }
